f388d1f9e369cd46ba2cee755abcdd3985dd5b52
Shing-Tung Yau <siqizhu4@illinois.edu>
2026-08-10 13:04:15 -0500

    Update link and remove image
    Updated link for Siqi Zhu and removed image reference.

--- src/config/People.jsx
+++ src/config/People.jsx
@@ -29,8 +29,7 @@ const phd_students = [
   },
   {
     name: "Siqi Zhu",
-    img: require("../assets/people/SiqiZhu.PNG"),
-    link: "https://sites.google.com/view/siqizhu/home",
+    link: "https://zhusq20.github.io/",
   },
 ];
 
